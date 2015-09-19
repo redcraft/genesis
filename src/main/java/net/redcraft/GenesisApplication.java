430abbe36e54cde98c88007390c11a595cea5ce1
Maxim Gurkin <redmax3d@gmail.com>
2015-09-20 04:30:40 +0800

    Adding initial project

---
+++ src/main/java/net/redcraft/GenesisApplication.java
@@ -0,0 +1,12 @@
+package net.redcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenesisApplication.class, args);
+    }
+}
